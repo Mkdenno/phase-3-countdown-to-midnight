10b29572aaf950765068b1a23aeb200e1e326735
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-08-17 10:58:20 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
-
-def countdown
+second_midnight=5
+def countdown(second_midnight)
+    while second_midnight > 0
+    puts "#{second_midnight} SECOND(S)!"
+    second_midnight -=1
+    end
+    "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(second_midnight)
+    while second_midnight > 0
+        puts "#{second_midnight} SECOND(S)!"
+        sleep(1)
+        second_midnight -=1
+
+    end
+    "HAPPY NEW YEAR!"
+
+end
+
+puts countdown(second_midnight)
+puts ""
+puts countdown_with_sleep(second_midnight)
